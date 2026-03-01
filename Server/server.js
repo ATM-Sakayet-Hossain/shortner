0a525de56c23d4d5a8096c83e891e496200704a1
ATM-Sakayet-Hossain <mdshakhawat434@gmail.com>
2026-03-02 00:18:05 +0600

    production domain

--- Server/server.js
+++ Server/server.js
@@ -4,10 +4,17 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const dbConfig = require("./dbConfig");
 require("dotenv").config();
-const Port = process.env.PORT || 1993
+const Port = process.env.PORT || 1993;
 
 const app = express();
-
+app.use(
+  cors({
+    origin: ["https://shortner-client.vercel.app"], // add your production domain later
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
