46e8d958777677ac7706aa7f262b1b416211de73
ATM-Sakayet-Hossain <mdshakhawat434@gmail.com>
2026-03-08 10:41:35 +0600

    authMiddleware

--- Server/middleware/authMiddleware.js
+++ Server/middleware/authMiddleware.js
@@ -1,10 +1,10 @@
-const { virifyToken } = require("../utils/token");
+const { verifyToken } = require("../utils/token");
 
 const IsAuthMiddleware = (req, res, next) => {
   try {
     const token = req.cookies?.acc_token;
-    const decoded = virifyToken(token);
-    req.user = decoded
+    const decoded = verifyToken(token);
+    req.user = decoded;
     next();
   } catch (error) {
     next();
@@ -13,12 +13,12 @@ const IsAuthMiddleware = (req, res, next) => {
 const authMiddleware = (req, res, next) => {
   try {
     const token = req.cookies?.acc_token;
-    const decoded = virifyToken(token);
-    if(!decoded) return res.status(401).send({message: "Unauthorized Request"})
-    req.user = decoded
+    const decoded = verifyToken(token);
+    if (!decoded) return res.status(401).send({ message: "Unauthorized Request" });
+    req.user = decoded;
     next();
   } catch (error) {
-   res.status(500).send({message: "Unauthorize Request"})
+    return res.status(401).send({ message: "Unauthorized Request" });
   }
 };
 
