3b0d48aaa29bf0bbed5b01e5c57c1a5fd3b66a26
ATM-Sakayet-Hossain <mdshakhawat434@gmail.com>
2026-03-08 10:51:15 +0600

    token

--- Server/utils/token.js
+++ Server/utils/token.js
@@ -1,14 +1,3 @@
-// const jwt = require("jsonwebtoken");
-// const generateAccTkn = (payload) => {
-//   const token = jwt.sign(payload, process.env.JWT_SECRET);
-//   return token
-// };
-// const virifyToken = (token) => {
-//   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//   return decoded
-// }
-
-// module.exports = { generateAccTkn, virifyToken };
 const jwt = require("jsonwebtoken");
 
 const generateAccTkn = (payload) => {
