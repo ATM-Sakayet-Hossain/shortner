00d709afebb62ad4b1ef5f96e5110375f3b926f6
ATM SAKAYET HOSSAIN <mdshakhawat434@gmail.com>
2026-03-28 01:00:03 +0600

    Fix syntax for setting request credentials

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -11,7 +11,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    config.credentials = "include;
+    config.credentials = "include";
     return config;
   },
   (error) => Promise.reject(error),
