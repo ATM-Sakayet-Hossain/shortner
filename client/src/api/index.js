eb6b9287bd95979bb961adf64ee8e917d665acf4
ATM SAKAYET HOSSAIN <mdshakhawat434@gmail.com>
2026-03-27 19:10:03 +0600

    Add credentials option to Axios instance

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:1993",
   withCredentials: true,
+  credentials: "include",
   headers: {
     "Content-Type": "application/json",
   },
